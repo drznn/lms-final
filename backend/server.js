ca3fba7dcea14fd12922c772f2671f3401c70a13
André Albuquerque <a.albuquerque@live.com>
2025-03-02 20:56:42 -0300

    estruturando frontend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const authRoutes = require("./routes/authRoutes");
+const feiraRoutes = require("./routes/feiraRoutes");
 
-app.use("/auth", authRoutes);
 // Configuração do servidor
+app.use("/feiras", feiraRoutes);
+app.use("/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
 
